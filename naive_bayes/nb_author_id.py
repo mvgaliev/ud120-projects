d68d96140ed3227265c4eae05e1f354bcfd94eeb
Maksim Galiev <v-makgal@microsoft.com>
2017-10-16 17:12:40 +0300

    added gaussian and svm methods

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+t0 = time()
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+pred = clf.predict(features_test)
+
+print accuracy_score(pred, labels_test)
 
 #########################################################
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,28 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+ 
 #########################################################
 ### your code goes here ###
+t0 = time()
+clf = SVC(kernel='rbf', C=10000.0)
+print "training started"
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+
+pred = clf.predict(features_test)
+
+count = 0
+
+for el in pred:
+    if el == 1:
+        count += 1
+
+print count
+##rint accuracy_score(pred, labels_test)
 #########################################################
 
 
